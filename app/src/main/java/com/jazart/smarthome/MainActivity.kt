0f947fe75112b326a1c47b75d282fec1a0c42bad
Mekaila <qmekaila@gmail.com>
2019-01-30 10:42:58 -0500

    mekaila push login screen

--- app/src/main/java/com/jazart/smarthome/MainActivity.kt
+++ app/src/main/java/com/jazart/smarthome/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.jazart.smarthome
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+        login_btn.setOnClickListener {
+            var status = if (username_et.text.toString().equals("SmartHome")
+                && password_et.text.toString().equals("home")
+            ) "Logged In Successfully" else "Login failed"
+
+            Toast.makeText(this, status, Toast.LENGTH_SHORT).show()
+        }
+    }
 }
