4de0c74b4daade7e7491e2792fdabf11439caf01
MQuarshi <qmekaila@gmail.com>
2019-02-11 17:47:02 -0500

    bottom sheet

--- app/src/main/java/com/jazart/smarthome/MainActivity.kt
+++ app/src/main/java/com/jazart/smarthome/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.jazart.smarthome
 
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 /**
  * Entry point of the application. This class sets up the navigation component and navigates to
@@ -39,6 +45,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun showBottomSheet() {
 
+        
     }
 
 
