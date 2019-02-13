212d1e7f3e940c0ef4e13c4f346bd4312c74642d
jazart <payne_jeremy@columbusstate.edu>
2019-02-13 10:54:24 -0500

    JP -moved vm module to it's own file

--- app/src/main/java/com/jazart/smarthome/di/AppModule.kt
+++ app/src/main/java/com/jazart/smarthome/di/AppModule.kt
@@ -31,26 +31,3 @@ interface AppComponent {
     }
     fun inject(app: App)
 }
-
-
-
-@MustBeDocumented
-@Target(
-    AnnotationTarget.FUNCTION,
-    AnnotationTarget.PROPERTY_GETTER,
-    AnnotationTarget.PROPERTY_SETTER
-)
-@Retention(AnnotationRetention.RUNTIME)
-@MapKey
-annotation class ViewModelKey(val value: KClass<out ViewModel>)
-
-@Module
-abstract class ViewModelModule {
-    @Binds
-    @IntoMap
-    @ViewModelKey(LoginViewModel::class)
-    abstract fun bindLoginViewModel(loginViewModel: LoginViewModel): ViewModel
-
-    @Binds
-    abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
-}
