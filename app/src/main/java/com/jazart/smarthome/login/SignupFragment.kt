6eab1dc394ed20820c93ca37824e85440790cfb7
jazart <payne_jeremy@columbusstate.edu>
2019-03-10 00:01:18 -0500

    JP signup validation

--- app/src/main/java/com/jazart/smarthome/login/SignupFragment.kt
+++ app/src/main/java/com/jazart/smarthome/login/SignupFragment.kt
@@ -4,19 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.jazart.smarthome.R
+import kotlinx.android.synthetic.main.fragment_login.*
 import kotlinx.android.synthetic.main.fragment_signup.*
-import java.lang.Exception
 
 class SignupFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_signup, container, false)
     }
-
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -27,26 +29,46 @@ class SignupFragment : Fragment() {
             )
             .load(R.drawable.signup)
             .into(image)
-        signUpButton.setOnClickListener{
+        setupTextListeners()
+        signUpButton.setOnClickListener {
             receivedInput()
         }
     }
 
-    fun receivedInput() : Boolean?{
+
+    fun receivedInput(): Boolean? {
         val receivedFirstName = firstNameEt.text.toString()
         val receivedLastName = lastNameEt.text.toString()
         val receivedEmail = emailEt.text.toString()
         val receivedPassword = passwordEt.text.toString()
         val receivedVerified = verifyPasswordEt.text.toString()
 
-        if (receivedPassword != receivedVerified){
+        if (receivedPassword != receivedVerified) {
             return null
         }
-
-        if(receivedFirstName.isBlank()){
-            firstName.error = "Please Complete this field"
-            firstName.isErrorEnabled = true
-        }
         return false
+    }
+
+    private fun setupTextListeners() {
+        firstNameEt.validate()
+        lastNameEt.validate()
+        passwordEt.validate()
+        verifyPasswordEt.validate()
+        emailEt.validate()
+        username_et.validate()
+    }
+}
+
+fun TextInputEditText.validate() {
+    val textInputLayout = this.parent as TextInputLayout
+    doOnTextChanged { text, start, count, after ->
+        if (text.isNullOrBlank()) {
+
+            textInputLayout.isErrorEnabled = false
+            textInputLayout.error = "Please Complete this field"
+        } else {
+            textInputLayout.isErrorEnabled = false
+
+        }
     }
 }
