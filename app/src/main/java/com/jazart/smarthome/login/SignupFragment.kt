05e8978626961a32f2e21ba66e8679b09fbbf5c2
jazart <payne_jeremy@columbusstate.edu>
2019-03-09 23:08:03 -0500

    JP -signup design

--- app/src/main/java/com/jazart/smarthome/login/SignupFragment.kt
+++ app/src/main/java/com/jazart/smarthome/login/SignupFragment.kt
@@ -5,11 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.jazart.smarthome.R
+import kotlinx.android.synthetic.main.fragment_signup.*
 
 class SignupFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_signup, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Glide.with(this)
+            .applyDefaultRequestOptions(
+                RequestOptions.fitCenterTransform()
+            )
+            .load(R.drawable.signup)
+            .into(image)
     }
 }
